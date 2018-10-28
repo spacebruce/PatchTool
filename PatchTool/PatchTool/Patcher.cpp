3e39c4788089550e3857da8e3612c0ce065ce565
bakagamedev <noel.abercrombie@hotmail.com>
2018-10-28 12:26:26 +0000

    Add Constructors and Path strings

--- PatchTool/PatchTool/Patcher.cpp
+++ PatchTool/PatchTool/Patcher.cpp
@@ -1,11 +1,19 @@
 #include "pch.h"
 #include "Patcher.h"
 
-
-Patcher::Patcher()
+Patcher::Patcher(char * RomPath, char * PatchPath, char * OutPath)
 {
+	this->RomPath = std::string(RomPath);
+	this->PatchPath = std::string(PatchPath);
+	this->OutPath = std::string(OutPath);
 }
 
+Patcher::Patcher(const std::string &RomPath, const std::string &PatchPath, const std::string &OutPath)
+{
+	this->RomPath = RomPath;
+	this->PatchPath = PatchPath;
+	this->OutPath = OutPath;
+}
 
 Patcher::~Patcher()
 {
--- PatchTool/PatchTool/Patcher.h
+++ PatchTool/PatchTool/Patcher.h
@@ -1,8 +1,13 @@
 #pragma once
+
+#include <string>
 class Patcher
 {
+	std::string RomPath, PatchPath, OutPath;
 public:
-	Patcher();
+	Patcher(char * RomPath, char * PatchPath, char * OutPath);
+	Patcher(const std::string &RomPath, const std::string &PatchPath, const std::string &OutPath);
+
 	~Patcher();
 };
 
