2705817eba7890ac8f76f0f5f6f4f83702eb2ce4
bakagamedev <noel.abercrombie@hotmail.com>
2018-10-30 21:58:01 +0000

    Copy-Paste run Method into UPSPatcher class

--- PatchTool/PatchTool/UPSPatcher.cpp
+++ PatchTool/PatchTool/UPSPatcher.cpp
@@ -21,10 +21,44 @@ static uintmax_t UPSPatcher::ReadVariableWidthInteger(const std::vector<CharT> &
 	return result;
 }
 
-UPSPatcher::UPSPatcher()
+UPSPatcher::UPSPatcher(std::vector<char> &RomFile, const std::vector<char>& PatchFile, std::size_t Position)
 {
+	this->RomFile = RomFile;
+	this->PatchFile = PatchFile;
+	this->Position = Position;
 }
 
-UPSPatcher::~UPSPatcher()
+void UPSPatcher::Run()
 {
+	std::cout << "UPS format isn't fully tested and may produce mangled files!\n";
+
+	std::size_t SourceSize = static_cast<std::size_t>(ReadVariableWidthInteger(PatchFile, Position));
+	std::size_t DestinationSize = static_cast<std::size_t>(ReadVariableWidthInteger(PatchFile, Position));
+
+	if (SourceSize != DestinationSize)
+	{
+		RomFile.resize(DestinationSize);
+	}
+
+	std::size_t RomPointer = 0;
+
+	while (RomPointer < DestinationSize)
+	{
+		std::size_t Skip = static_cast<std::size_t>(ReadVariableWidthInteger(PatchFile, Position));
+		RomPointer += Skip;
+
+		std::vector<char> Chunk;
+		do
+		{
+			Chunk.emplace_back(PatchFile[Position]);
+			++Position;
+		} while (PatchFile[Position] != 0);
+
+		for (std::size_t i = 0; i < Chunk.size(); ++i)
+		{
+			RomFile[RomPointer] = RomFile[RomPointer] ^ Chunk[i];
+			++RomPointer;
+		}
+	}
+	//skip checksums for now
 }
