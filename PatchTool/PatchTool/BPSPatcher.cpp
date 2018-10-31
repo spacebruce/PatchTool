d2aa472903cc196ea0d98e9c7ba07193e72414a9
bakagamedev <noel.abercrombie@hotmail.com>
2018-10-31 12:06:35 +0000

    Copy ReadVariableWidthInteger from UPS mode
    put them both somewhere common later

--- PatchTool/PatchTool/BPSPatcher.cpp
+++ PatchTool/PatchTool/BPSPatcher.cpp
@@ -1,6 +1,26 @@
 #include "pch.h"
 #include "BPSPatcher.h"
 
+
+// ?????
+template <typename CharT>
+static uintmax_t BPSPatcher::ReadVariableWidthInteger(const std::vector<CharT> &Data, std::size_t &Position)
+{
+	uintmax_t result = 0, shift = 0;
+	for (;;) {
+		uint8_t octet = Data[Position];
+		++Position;
+		if (octet & 0x80) {
+			result += (octet & 0x7f) << shift;
+			break;
+		}
+		result += (octet | 0x80) << shift;
+		shift += 7;
+	}
+
+	return result;
+}
+
 BPSPatcher::BPSPatcher(std::vector<char>& RomFile, const std::vector<char>& PatchFile, std::size_t Position)
 {
 	this->RomFile = RomFile;
--- PatchTool/PatchTool/BPSPatcher.h
+++ PatchTool/PatchTool/BPSPatcher.h
@@ -3,6 +3,10 @@
 
 class BPSPatcher : public PatchBase
 {
+private:
+	// ?????
+	template <typename CharT>
+	static uintmax_t ReadVariableWidthInteger(const std::vector<CharT> &Data, std::size_t &Position);
 public:
 	BPSPatcher(std::vector<char> &RomFile, const std::vector<char>& PatchFile, std::size_t Position);
 	std::vector<char> Run();
