803a90d294d55802c639964e13e4ec5e5a73ad44
bakagamedev <noel.abercrombie@hotmail.com>
2018-10-31 13:41:20 +0000

    Tidy code

--- PatchTool/PatchTool/PatchBase.h
+++ PatchTool/PatchTool/PatchBase.h
@@ -9,7 +9,7 @@ class PatchBase
 protected:	//oh so THAT's what protected does. Today I learned.
 	std::string RomPath, PatchPath, OutPath;
 	std::vector<char> RomFile, PatchFile;
-	std::size_t Position;
+	std::size_t Position = 0;
 public:
 	virtual ~PatchBase() = 0;
 	virtual std::vector<char> Run() = 0;
